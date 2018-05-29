89b97630f61d67a8e7d82a9c3970b6483f96aa3d
Jordan  Haddadi <jordanhaddadi@gmail.com>
2018-05-29 21:33:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function deleteFromObjectByKey (object, key) {
 }
 
 function desctructivelyDeleteFromObjectByKey (object, key) {
-  delete object[key];
+  object[key] = value;
   return object;
 }
